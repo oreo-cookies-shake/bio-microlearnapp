711b720a093c20dc1749c465a1bf49f6b43a034b
oreo-cookies-shake <ting.wrk@gmail.com>
2026-01-15 16:11:07 +0000

    changed back whole css

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change the v9 to v10 to force your phone to update!
-const CACHE_NAME = "microlearn-bio-v10"; 
+const CACHE_NAME = "microlearn-bio-v11"; 
 
 const ASSETS = [
   "./",
@@ -36,4 +36,4 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
-});git add .
+});
