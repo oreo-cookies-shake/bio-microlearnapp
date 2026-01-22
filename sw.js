ed45834cb8fc268966a04c76ff92cd86bbfe2819
oreo-cookies-shake <ting.wrk@gmail.com>
2026-01-22 18:18:24 +0800

    Update PWA manifest and service worker

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
 // Bump the version to force PWAs to update on phones.
-const CACHE_NAME = "microlearn-bio-v46";
+const CACHE_NAME = "microlearn-bio-v47";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./theme.css",
-  "./base.css",
-  "./modes.css",
-  "./components.css",
-  "./script.js",
-  "./manifest.webmanifest"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/theme.css",
+  "/base.css",
+  "/modes.css",
+  "/components.css",
+  "/script.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/icons/maskable-192.png",
+  "/icons/maskable-512.png",
+  "/icons/apple-touch-icon.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -28,5 +33,32 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("/index.html"))
+    );
+    return;
+  }
+
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request).then((networkResponse) => {
+          if (networkResponse && networkResponse.ok) {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          }
+          return networkResponse;
+        });
+      })
+    );
+  }
 });
